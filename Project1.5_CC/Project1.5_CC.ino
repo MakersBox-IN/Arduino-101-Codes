b5bbeccff4b22df83e9d53b1929d02daa09ecd91
MakersBox <care@makersbox.in>
2016-02-10 11:48:45 +0530

    Create Project1.5_CC.ino

---
+++ Project1.5_CC/Project1.5_CC.ino
@@ -0,0 +1,41 @@
+int redPin = 9;    
+int greenPin = 10;   
+int bluePin = 11;   
+
+void setup()
+{
+
+pinMode(redPin, OUTPUT);   
+pinMode(greenPin, OUTPUT);   
+pinMode(bluePin, OUTPUT);   
+}
+
+//function to set colour on the led
+void color (int red, int green, int blue)
+{
+analogWrite(redPin, red);
+analogWrite(bluePin, blue);
+analogWrite(greenPin, green);
+}
+
+void loop()   
+{
+  color(255, 0, 0);   
+  delay(500);    
+  color(0, 255, 0);    
+  delay(500);    
+  color(0, 0, 255);   
+  delay(500);   
+  color(255, 255, 0);    
+  delay(500);    
+  color(255 , 255, 255);  
+  delay(500);  
+  color(128, 0, 255);   
+  delay(500);    
+  color(175, 75, 148);  
+  delay(500);    
+  color(237, 119, 6);   
+  delay(500);   
+  color(0, 0, 0);    
+  delay(1000);  
+}
