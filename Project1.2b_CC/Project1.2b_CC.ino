1e7c5b68418abe4e7d4aab919b20e25dfa121576
MakersBox <care@makersbox.in>
2016-02-10 11:30:21 +0530

    Create Project1.2b_CC.ino

---
+++ Project1.2b_CC/Project1.2b_CC.ino
@@ -0,0 +1,21 @@
+// the setup function runs once when you press reset or power the board
+
+void setup() {
+
+  // initialize digital pin 7 as an output.
+
+  pinMode(7, OUTPUT);
+
+}
+
+// the loop function runs over and over again forever
+
+void loop() {
+
+  // turn the LED off (Since the LED used is a common cathode LED, LOW signal turns the LED off)
+
+  digitalWrite(7, LOW;   
+
+  // for common anode type LED signal would be HIGH instead of LOW to turn off the LED  
+
+}
