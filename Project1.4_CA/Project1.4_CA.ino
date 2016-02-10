b645513bf5586fc12d3d30108470fb6fe651d45b
MakersBox <care@makersbox.in>
2016-02-10 11:35:49 +0530

    Create Project1.4_CA.ino

---
+++ Project1.4_CA/Project1.4_CA.ino
@@ -0,0 +1,56 @@
+//assigning variables to pin numbers as names are easy to remember
+int redPin = 9;    
+int greenPin = 10;   
+int bluePin = 11;   
+
+void setup()
+{
+pinMode(redPin, OUTPUT);   
+pinMode(greenPin, OUTPUT);   
+pinMode(bluePin, OUTPUT);   
+}
+
+//function to cycle through all brightness levels of red coloured led
+void red()
+{
+  //looping through all brightness levels
+  for(int i=255;i>=0;i-=5)
+  {analogWrite(redPin, i);
+  analogWrite(greenPin, 255);
+  analogWrite(bluePin, 255);
+  //delay of 200 milliseconds between each brightness level
+  delay(200);
+  }
+}
+
+//function to cycle through all brightness levels of blue coloured led
+void blue()
+{
+  for(int i=255;i>=0;i-=5)
+  {analogWrite(bluePin, i);
+  analogWrite(greenPin, 255);
+  analogWrite(redPin, 255);
+  delay(200);
+  }
+}
+
+//function to cycle through all brightness levels of green coloured led
+void green()
+{
+  for(int i=255;i>=0;i-=5)
+  {analogWrite(greenPin, i);
+  analogWrite(redPin, 255);
+  analogWrite(bluePin, 255);
+  delay(200);
+  }
+}
+
+void loop()   
+{
+  red();
+  delay(500);
+  green();
+  delay(500);
+  blue();
+  delay(500);
+}
